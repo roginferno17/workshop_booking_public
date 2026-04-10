d5ad2d3336a482f97893d8a46b7eb02b7b0aa543
web97 <darshan.j@moontechnolabs.com>
2026-04-10 13:47:54 +0530

    Remove tracked virtualenv files and ignore local history

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Krish\Documents\GitHub\workshop_booking\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
